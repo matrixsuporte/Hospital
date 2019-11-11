385becfb64963a7a9e4da9056667c807d6a49269
matrixsuporte <remap@matrixsuporte>
2019-11-11 15:03:20 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TemplateAdminLTE2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
