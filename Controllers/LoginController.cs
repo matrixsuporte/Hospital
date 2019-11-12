32f4c0b927e2e5647027ef454e8c47c63c8e4e2e
matrixsuporte <remap@matrixsuporte>
2019-11-12 11:38:43 -0300

    Novas Funcionalidade adicionadas: 1 - scaffold para criar modelos do banco de dados. 2 - tela de login funcionando, com usuario e senha do banco.

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hospital.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Hospital.Controllers
 {
     public class LoginController : Controller
     {
+        MyDbContext db = new MyDbContext();
         public IActionResult Login()
         {
             return View();
+        }
+        public ActionResult Validate(Admins admin)
+        {
+            var _admin = db.Admins.Where(s => s.Email == admin.Email);
+            if (_admin.Any()) {
+                if (_admin.Where(s => s.Password == admin.Password).Any()) {
+                    return Json(new { status = true, message = "Login com Sucesso!"});
+                }
+                else
+                {
+                    return Json(new { status = false, message = "Senha Inválida!"});
+                }
+            }
+            else
+            {
+                return Json(new { status = false, message = "E-mail Inválido!"});
+            }
         }
     }
 }
